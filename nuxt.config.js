8e32a81195a8980ee651ddd35cdd8918b6a3d252
Alex <a.nyashukov@gmail.com>
2022-04-12 14:08:23 +0300

    split apps

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-var dev = process.env.NODE_ENV === 'development'
+var dev = process.env.NODE_ENV === 'development';
 
 console.log(process.env);
 
@@ -6,7 +6,7 @@ module.exports = {
   mode: 'spa',
   server: {
     host: '0.0.0.0',
-    port: 8080
+    port: 8080,
   },
   head: {
     titleTemplate: '%s',
@@ -14,32 +14,27 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || '',
+      },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   loading: { color: '#fff' },
-  css: [
-  ],
+  css: [],
   plugins: [
     // '@plugins/app',
     // '@plugins/composition-api'
   ],
-  buildModules: [
-    '@nuxtjs/vuetify',
-  ],
-  modules: [
-    '@nuxtjs/axios'
-  ],
+  buildModules: ['@nuxtjs/vuetify'],
+  modules: ['@nuxtjs/axios'],
   axios: {
-    baseURL:  dev ? '' : 'http://151.248.113.166'
+    baseURL: dev ? '' : process.env.URL,
   },
-  vuetify: {
-  },
+  vuetify: {},
   build: {
-    extend (config, ctx) {
-    }
-  }
-}
+    extend(config, ctx) {},
+  },
+};
--- server/app.js
+++ server/app.js
@@ -1,143 +1,152 @@
-const mongoose = require('mongoose')
-const express = require('express')
-const bodyParser = require('body-parser')
-const moment = require('moment')
-const createModels = require('./createModels')
-const createCrudRouter = require('./createCrudRouter')
-const fs = require('fs')
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const moment = require('moment');
+const createModels = require('./createModels');
+const createCrudRouter = require('./createCrudRouter');
+const fs = require('fs');
 
-const { statusesByDate } = require('../vendor/graphic_utils')
+const { statusesByDate } = require('../vendor/graphic_utils');
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
-mongoose.set('useFindAndModify', false)
-const conn = mongoose.createConnection('mongodb://localhost/mgt',
+mongoose.set('useFindAndModify', false);
+const conn = mongoose.createConnection(
+  process.env.MONGODB_URL ?? 'mongodb://localhost/mgt',
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   }
-)
+);
 
-createModels(conn)
+createModels(conn);
 
-app.set('mongoose', conn)
+app.set('mongoose', conn);
 
-app.use('/drivers', createCrudRouter('driver', ['bus']))
-app.use('/buses', createCrudRouter('bus', ['way']))
-app.use('/routes', createCrudRouter('route'))
-app.use('/ways', createCrudRouter('way', ['route']))
+app.use('/drivers', createCrudRouter('driver', ['bus']));
+app.use('/buses', createCrudRouter('bus', ['way']));
+app.use('/routes', createCrudRouter('route'));
+app.use('/ways', createCrudRouter('way', ['route']));
 
 app.post('/report', async (req, res) => {
-  const Bus = res.app.get('mongoose').models['bus']
-  let buses = await Bus.find().populate('drivers')
-  buses = buses.sort((a, b) => a.num - b.num)
-    .map(bus => ({
+  const Bus = res.app.get('mongoose').models['bus'];
+  let buses = await Bus.find().populate('drivers');
+  buses = buses
+    .sort((a, b) => a.num - b.num)
+    .map((bus) => ({
       _id: bus._id,
       num: bus.num,
       status: bus.status,
-      drivers: bus.drivers.map(driver => {
-        const status = statusesByDate(driver.graphic, req.body.date, 1)[0]
+      drivers: bus.drivers.map((driver) => {
+        const status = statusesByDate(driver.graphic, req.body.date, 1)[0];
         return {
           _id: driver._id,
           num: driver.num,
           name: driver.shortName,
-          status: status.exception || driver.graphic.status || status.value
-        }
-      })
-    }))
+          status: status.exception || driver.graphic.status || status.value,
+        };
+      }),
+    }));
 
   const report = {
     rows: [],
     buses: {
-      'Ремонт': [],
-      'СВАРЗ': [],
-      'Резерв': []
+      Ремонт: [],
+      СВАРЗ: [],
+      Резерв: [],
     },
     drivers: {
-      'В': [],
-      'Б': [],
-      'О': [],
-      'Резерв': []
-    }
-  }
+      В: [],
+      Б: [],
+      О: [],
+      Резерв: [],
+    },
+  };
 
-  buses.forEach(bus => {
+  buses.forEach((bus) => {
     const row = {
       bus: null,
       first: null,
       second: null,
-      full: null
-    }
+      full: null,
+    };
 
     if (!['Ремонт', 'СВАРЗ'].includes(bus.status)) {
-      row.bus = bus
+      row.bus = bus;
     } else {
-      report.buses[bus.status].push(bus)
+      report.buses[bus.status].push(bus);
     }
 
-    bus.drivers.forEach(driver => {
+    bus.drivers.forEach((driver) => {
       if (['В', 'Б', 'О'].includes(driver.status)) {
-        report.drivers[driver.status].push(driver)
+        report.drivers[driver.status].push(driver);
 
         if (driver.status == 'В') {
-          row.weekday = driver
+          row.weekday = driver;
         }
-        return
+        return;
       }
 
       if (driver.status == '1') {
-        row.first = driver
+        row.first = driver;
       } else if (driver.status == '2') {
-        row.second = driver
+        row.second = driver;
       } else if (driver.status == 'Р') {
-        row.full = driver
+        row.full = driver;
       }
 
       if (row.bus == null) {
-        report.drivers['Резерв'].push(driver)
+        report.drivers['Резерв'].push(driver);
       }
-    })
-    if (row.first == row.second == row.full == null && row.bus != null) {
-      report.buses['Резерв'].push(bus)
+    });
+    if (((row.first == row.second) == row.full) == null && row.bus != null) {
+      report.buses['Резерв'].push(bus);
     }
 
-    report.rows.push(row)
-  })
+    report.rows.push(row);
+  });
 
-  res.json(report)
-})
+  res.json(report);
+});
 
 app.post('/magazine', async (req, res) => {
-  const Bus = res.app.get('mongoose').models['bus']
-  let buses = await Bus.find().populate({ path: 'way', populate: 'route' }).populate('drivers')
-  buses = buses.sort((a, b) => a.num - b.num)
+  const Bus = res.app.get('mongoose').models['bus'];
+  let buses = await Bus.find()
+    .populate({ path: 'way', populate: 'route' })
+    .populate('drivers');
+  buses = buses.sort((a, b) => a.num - b.num);
 
-  let daysInMonth = moment(req.body.month, "YYYY-MM").daysInMonth()
+  let daysInMonth = moment(req.body.month, 'YYYY-MM').daysInMonth();
 
   const weekdays = Array.from(
     {
-      length: moment(req.body.month, "YYYY-MM").daysInMonth()
+      length: moment(req.body.month, 'YYYY-MM').daysInMonth(),
     },
-    (x, i) => moment(req.body.month, "YYYY-MM")
-      .startOf('month')
-      .add(i, 'days')
-  ).filter(d => [6, 0].includes(d.day()))
-    .map(d => d.format('D'))
+    (x, i) =>
+      moment(req.body.month, 'YYYY-MM')
+        .startOf('month')
+        .add(i, 'days')
+  )
+    .filter((d) => [6, 0].includes(d.day()))
+    .map((d) => d.format('D'));
 
-
-  buses = buses.map(bus => ({
+  buses = buses.map((bus) => ({
     num: bus.num,
     way: bus.way,
-    drivers: bus.drivers.map(driver => ({
+    drivers: bus.drivers.map((driver) => ({
       num: driver.num,
       name: driver.shortName,
       firstDayRest: driver.firstDayRest,
       fullname: driver.name,
       graphic: driver.graphic,
-      statuses: statusesByDate(driver.graphic, req.body.month + '-01', daysInMonth)
-    }))
-  }))
+      statuses: statusesByDate(
+        driver.graphic,
+        req.body.month + '-01',
+        daysInMonth
+      ),
+    })),
+  }));
 
   const norm = +req.body.norm || 0;
   for (const bus of buses) {
@@ -148,13 +157,13 @@ app.post('/magazine', async (req, res) => {
           if (driver.firstDayRest) {
             statuses.reverse();
           }
-  
+
           for (let i = 0; i < statuses.length - 1; i++) {
             if (statuses[i].value === 'В') {
               if (i === 0 && statuses[i + 1].value !== 'В') {
                 statuses[i].value = 'В'; // О
               }
-  
+
               if (statuses[i + 1].value === 'В') {
                 statuses[i + 1].value = 'В'; // О
               }
@@ -165,39 +174,41 @@ app.post('/magazine', async (req, res) => {
             statuses.reverse();
           }
         }
-      } catch (e) { }
+      } catch (e) {}
 
       try {
-        const workStatuses = driver.statuses.filter(s => ['Р', '1', '2'].includes(s.value));
+        const workStatuses = driver.statuses.filter((s) =>
+          ['Р', '1', '2'].includes(s.value)
+        );
         const workMinutes = workStatuses.reduce((acc, cur) => {
           try {
             let time;
             switch (cur.value) {
               case 'Р':
-                time = bus.way.times.durationFirstSmene.split(':')
+                time = bus.way.times.durationFirstSmene.split(':');
                 return acc + +time[1] + +time[0] * 60;
               case '1':
-                time = bus.way.times.durationFirstSmene.split(':')
+                time = bus.way.times.durationFirstSmene.split(':');
                 return acc + +time[1] + +time[0] * 60;
               case '2':
-                time = bus.way.times.durationSecondSmene.split(':')
+                time = bus.way.times.durationSecondSmene.split(':');
                 return acc + +time[1] + +time[0] * 60;
             }
           } catch (e) {
             return acc;
-          };
+          }
           return acc;
         }, 0);
         const workHours = workMinutes / 60;
 
         const needRate = [
           Math.floor(norm / workStatuses.length),
-          Math.floor(((norm / workStatuses.length) % 1) * 60)
+          Math.floor(((norm / workStatuses.length) % 1) * 60),
         ];
 
         const currRate = [
           Math.floor(workHours / workStatuses.length),
-          Math.floor(((workHours / workStatuses.length) % 1) * 60)
+          Math.floor(((workHours / workStatuses.length) % 1) * 60),
         ];
 
         driver.rates = {
@@ -205,16 +216,16 @@ app.post('/magazine', async (req, res) => {
           currRate: currRate.join(':'),
           currTotal: workHours,
           over: workHours - norm,
-          isCritic: ((norm + 11) - workHours) <= 0
-        }
+          isCritic: norm + 11 - workHours <= 0,
+        };
       } catch (e) {
         continue;
       }
     }
   }
 
-  const magazine = { pages: [] }
-  const busesPerPage = req.body.busesPerPage || 4
+  const magazine = { pages: [] };
+  const busesPerPage = req.body.busesPerPage || 4;
 
   for (
     let i = 0, pageNumber = magazine.pages.length;
@@ -224,11 +235,11 @@ app.post('/magazine', async (req, res) => {
     magazine.pages.push({
       weekdays,
       buses: buses.slice(i, i + busesPerPage),
-      number: pageNumber
-    })
+      number: pageNumber,
+    });
   }
 
-  res.json(magazine)
-})
+  res.json(magazine);
+});
 
-module.exports = app
+module.exports = app;
